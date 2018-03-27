f075a7f79e4ef6f822c37cefa010449b4f8bd5b7
Rafiqul Hasan <rafiqul@finder-lbs.com>
2018-03-27 20:52:02 +0600

    config updated

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,8 +2,7 @@ module.exports = {
   siteMetadata: {
     title: "Rafiqul's Blog",
     author: 'Rafiqul Hasan',
-    description: 'A starter blog demonstrating what Gatsby can do.',
-    siteUrl: 'http://rafiqul.rocks/',
+    siteUrl: 'https://rafiqul.netlify.com/',
   },
   pathPrefix: '/',
   plugins: [
@@ -63,17 +62,6 @@ module.exports = {
         showSpinner: false,
       },
     },
-    {
-      resolve: `gatsby-plugin-netlify`,
-      options: {
-        headers: {}, // option to add more headers. `Link` headers are transformed by the below criteria
-        allPageHeaders: [], // option to add headers for all pages. `Link` headers are transformed by the below criteria
-        mergeSecurityHeaders: true, // boolean to turn off the default security headers
-        mergeLinkHeaders: true, // boolean to turn off the default gatsby js headers
-        mergeCachingHeaders: true, // boolean to turn off the default caching headers
-        transformHeaders: (headers, path) => headers, // optional transform for manipulating headers under each path (e.g.sorting), etc.
-        generateMatchPathRewrites: true, // boolean to turn off automatic creation of redirect rules for client only paths
-      },
-    },
+    `gatsby-plugin-netlify`,
   ],
 }

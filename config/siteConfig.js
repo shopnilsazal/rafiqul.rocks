29226e04021fbfc30428dc4da6c691632859825d
Rafiqul Hasan <rafiqul@finder-lbs.com>
2018-03-28 20:37:49 +0600

    disqus issue fixed

--- config/siteConfig.js
+++ config/siteConfig.js
@@ -1,5 +1,6 @@
 module.exports = {
   siteLogo: '/favicon.png', // Used for SEO and manifest
   siteBanner: '/favicon.png',
+  siteUrl: 'https://rafiqul.rocks/',
   disqusShortname: 'rafiqul-rocks',
 }

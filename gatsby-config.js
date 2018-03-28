7022cd293d1766e66df56110da5baa4e0009d7be
Rafiqul Hasan <rafiqul@finder-lbs.com>
2018-03-28 23:22:00 +0600

    google analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -41,7 +41,7 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
+        trackingId: `UA-116587623-1`,
       },
     },
     `gatsby-plugin-feed`,

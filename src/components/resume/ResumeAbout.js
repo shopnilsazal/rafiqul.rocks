71b716cf35c3b04d95012ee897d35269e075a420
Rafiqul Hasan <rafiqul@finder-lbs.com>
2018-08-08 23:16:49 +0600

    about info updated in resume

--- src/components/resume/ResumeAbout.js
+++ src/components/resume/ResumeAbout.js
@@ -3,7 +3,7 @@ import React from 'react'
 const ResumeAbout = () => (
   <div className="about">
     <p>
-      I am an avid learner, explorer, team player and self-motivated software
+      I am a fast and avid learner, explorer, team player and self-motivated software
       artisan with 4+ years’ experience. I enjoy taking on new challenges and
       constantly strives to learn new skills. I punch code that is reliable and
       useful.
